cea469ec91dd75edf2b26441b684aa9bc47f63d9
Fabio Oliveira <fabiotavares19@hotmail.com>
2020-09-09 15:44:49 -0300

    Users and Courses migrations added

---
+++ src/shared/infra/typeorm/migrations/1599674067280-CreateCourse.ts
@@ -0,0 +1,47 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateCourse1599674067280 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'courses',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'image',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'overview',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('courses');
+  }
+}
