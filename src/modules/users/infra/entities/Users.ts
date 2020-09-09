82603a039d9bedfc26653b8d99757a993319eb4f
Fabio Oliveira <fabiotavares19@hotmail.com>
2020-09-09 11:13:52 -0300

    users entity added

--- src/modules/users/infra/entities/Users.ts
+++ src/modules/users/infra/entities/Users.ts
@@ -1,11 +1,30 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
 class Users {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   email: string;
 
+  @Column()
   name: string;
 
+  @Column()
   password: string;
+
+  @CreateDateColumn()
+  created_at: string;
+
+  @UpdateDateColumn()
+  updated_at: string;
 }
 
 export default Users;
