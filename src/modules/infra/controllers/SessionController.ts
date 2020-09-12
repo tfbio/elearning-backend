c1ccf8e0be4055e630465badc0645a2b9d74409a
Fabio Oliveira <fabiotavares19@hotmail.com>
2020-09-12 10:20:05 -0300

    SessionController ajustment

--- src/modules/infra/controllers/SessionController.ts
+++ src/modules/infra/controllers/SessionController.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
 import LoginUserService from '../../services/LoginUserService';
 
 export default class SessionsController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { email, password } = request.body;
 
-    const authentication = new LoginUserService();
+    const authentication = container.resolve(LoginUserService);
 
     const { user_id, token } = await authentication.execute({
       email,
